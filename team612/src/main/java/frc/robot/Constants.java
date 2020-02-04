6d685ca6572ee2a8c7bd3869cb681a6e3681dee8
unknown <anoopdavid@outlook.com>
2020-02-04 08:52:47 -0500

    input port values for sparks/motors.

--- team612/src/main/java/frc/robot/Constants.java
+++ team612/src/main/java/frc/robot/Constants.java
@@ -3,10 +3,10 @@ package frc.robot;
 public final class Constants {
 
     //Spark Ports
-    public static final int SPARK_INTAKE = 1;
-    public static final int SPARK_UPPER_BELT = 2;
-    public static final int SPARK_LOWER_BELT = 7;
-    public static final int SPARK_OUTTAKE = 3;
+    public static final int SPARK_INTAKE = 9;
+    public static final int SPARK_UPPER_BELT = 8;
+    public static final int SPARK_LOWER_BELT = 6;
+    public static final int SPARK_OUTTAKE = 7;
     public static final int SOLENOID_L_INTAKE_FORWARD = 4;
     public static final int SOLENOID_R_INTAKE_FORWARD = 5;
     public static final int SOLENOID_ARM_FORWARD = 6;
