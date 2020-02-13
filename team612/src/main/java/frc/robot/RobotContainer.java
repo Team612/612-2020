60ee1bf95ed7374c8cd9631231de018bf851824e
unknown <anoopdavid@outlook.com>
2020-02-13 15:32:43 -0500

    Fixed last push

--- team612/src/main/java/frc/robot/RobotContainer.java
+++ team612/src/main/java/frc/robot/RobotContainer.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.autonomous.SampleAuto;
 import frc.robot.commands.climb.EngageClimb;
+import frc.robot.commands.climb.ReverseWinch;
 import frc.robot.commands.climb.RunWinch;
 import frc.robot.commands.climb.ToggleHook;
 import frc.robot.commands.drivetrain.DefaultDrive;
@@ -29,7 +30,7 @@ public class RobotContainer {
     ControlMap.driver_button_X.whenPressed(new EngageClimb(m_climb));
     ControlMap.driver_button_B.whenPressed(new ToggleHook(m_climb));
     ControlMap.driver_button_RB.whileHeld(new RunWinch(m_climb));
-    controlMap.driver_button_LB.whileHeld(new ReverseWinch(m_climb));
+    ControlMap.driver_button_LB.whileHeld(new ReverseWinch(m_climb));
   }
 
   // Put all default commands here
